d530bf90317a14e176d0367d9316e3f35e82d272
carlobrok <carlo.brokering@outlook.de>
2020-03-02 22:50:49 +0100

    Using libi2c-dev

--- KamelDevices.cpp
+++ KamelDevices.cpp
@@ -4,8 +4,8 @@
 #include <cerrno>							// errno
 #include <cstring>						// strerror
 #include <linux/i2c-dev.h>		// i2c_smbus_...
-#include <pca9685.h>
-#include <wiringPiI2C.h>
+//#include <pca9685.h>
+//#include <wiringPiI2C.h>
 #include <iostream>
 
 #include "KamelDevices.h"
@@ -38,19 +38,41 @@ int kamelI2Copen(int devId) {
 
 #define PCA9685_MODE1 0x0
 
-// Function copied from https://github.com/Reinbert/pca9685 library, modified for specific usage
-// Does not setup the wiringPi node, only works with the "advanced" functions of the library
 int pca9685_setup(int address) {
   int fd = kamelI2Copen(address);
   if (fd < 0) return fd;
 
   // Setup the chip. Enable auto-increment of registers.
-	int settings = wiringPiI2CReadReg8(fd, PCA9685_MODE1) & 0x7F;
+	int settings = i2c_smbus_read_byte_data(fd, PCA9685_MODE1) & 0x7F;
 	int autoInc = settings | 0x20;
 
-	wiringPiI2CWriteReg8(fd, PCA9685_MODE1, autoInc);
+	i2c_smbus_write_byte_data(fd, PCA9685_MODE1, autoInc);
 
-  pca9685PWMFreq(fd, 50);
+
+	// Set the frequency
+
+	// Min freq = 40, max freq = 1000
+	int freq = 50;
+
+	// To set pwm frequency we have to set the prescale register. The formula is:
+	// prescale = round(osc_clock / (4096 * frequency))) - 1 where osc_clock = 25 MHz
+	// Further info here: http://www.nxp.com/documents/data_sheet/PCA9685.pdf Page 24
+	int prescale = (int)(25000000.0f / (4096 * freq) - 0.5f);
+
+	// Get settings and calc bytes for the different states.
+	int settings = i2c_smbus_read_byte_data(fd, PCA9685_MODE1) & 0x7F;	// Set restart bit to 0
+	int sleep	= settings | 0x10;									// Set sleep bit to 1
+	int wake 	= settings & 0xEF;									// Set sleep bit to 0
+	int restart = wake | 0x80;										// Set restart bit to 1
+
+	// Go to sleep, set prescale and wake up again.
+	i2c_smbus_write_byte_data(fd, PCA9685_MODE1, sleep);
+	i2c_smbus_write_byte_data(fd, PCA9685_PRESCALE, prescale);
+	i2c_smbus_write_byte_data(fd, PCA9685_MODE1, wake);
+
+	// Now wait a millisecond until oscillator finished stabilizing and restart PWM.
+	delay(1);
+	i2c_smbus_write_byte_data(fd, PCA9685_MODE1, restart);
 
   return fd;
 }
@@ -79,9 +101,21 @@ Servo::~Servo() {
 void Servo::set_angle(int angle) {
   int off = (int)(4096 * ((angle*1.0 / m_max_angle) * ((m_max_ms - m_min_ms) / 20.0) + (m_min_ms / 20.0)) + 0.5f);
 
-  pca9685PWMWrite(m_fd, m_pin, 0, off);
+  //pca9685PWMWrite(m_fd, m_pin, 0, off);
+
+	// Write to on and off registers and mask the 12 lowest bits of data to overwrite full-on and off
+	i2c_smbus_write_word_data(fd, get_register(m_pin), 0 & 0x0FFF);
+	i2c_smbus_write_word_data(fd, get_register(m_pin) + 2, off & 0x0FFF);
 }
 
 void Servo::off() {
-  pca9685FullOff(m_fd, m_pin, 1);
+  //pca9685FullOff(m_fd, m_pin, 1);
+
+	int reg = get_register(m_pin) + 3;		// LEDX_OFF_H
+	int state = i2c_smbus_read_byte_data(fd, reg);
+
+	// Set bit 4 to 1 or 0 accordingly
+	state |= 0x10;
+
+	i2c_smbus_write_byte_data(fd, reg, state);
 }
